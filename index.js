ebcefb97ebab11785d6f02a32498dc99d9c66640
Fabian Beuke <mail@beuke.org>
2017-07-01 00:13:04 +0200

    clean up code

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const today = moment().format('YYYY-MM-DD')
 const q = moment().subtract(120, 'days').format('YYYY-MM-DD')
 console.log(today)
 console.log(q)
-const url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${q}&end=${today}`
+const coindesk = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${q}&end=${today}`
 
-axios.get(url)
-  .then(function (response) {
-    const x = _.get('data.bpi')(response)
-    const y = _.values(x)
-    console.log(y.length)
-    console.log (asciichart.plot (y,{ height: 14 }))
-  })
-  .catch(function (error) {
-    console.log(error)
-  })
+const bitcoin = async (url) => {
+  const res = await axios.get(url)
+    const data = _.flow(
+      _.get('data.bpi'),
+      _.values
+    )(res)
+    console.log(data.length)
+    console.log (asciichart.plot (data, { height: 14 }))
+}
+
+bitcoin(coindesk)
 
