5ec0807c0f996a779a096537c40da4115546ac03
Fabian Beuke <mail@beuke.org>
2017-07-01 01:07:17 +0200

    add legend

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ const _ = require('lodash-fp')
 const axios = require('axios')
 const moment = require('moment')
 const asciichart = require ('asciichart')
-const today = moment().format('YYYY-MM-DD')
 
-const q = moment().subtract(120, 'days').format('YYYY-MM-DD')
-console.log(today)
-console.log(q)
+const today = moment().format('YYYY-MM-DD')
+const q = moment().subtract(90, 'days').format('YYYY-MM-DD')
 const coindesk = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${q}&end=${today}`
 
-const bitcoin = async (url) => {
+const bitcoin = async url => {
     const res = await axios.get(url)
     const data = _.flow(
         _.get('data.bpi'),
         _.values
     )(res)
-    console.log(data.length)
     console.log (asciichart.plot (data, { height: 14 }))
+    console.log(`\t\t Bitcoin Chart from ${q} to ${today}`)
 }
 
 bitcoin(coindesk)
