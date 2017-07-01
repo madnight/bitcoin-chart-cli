98675fd2f8a90a5d0640769547eae40ceafbbad0
Fabian Beuke <mail@beuke.org>
2017-07-01 02:09:00 +0200

    add maxWidth option

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const _ = require('lodash-fp')
+const l = require('lodash')
 const axios = require('axios')
 const moment = require('moment')
 const asciichart = require ('asciichart')
 
 const today = moment().format('YYYY-MM-DD')
-const q = moment().subtract(90, 'days').format('YYYY-MM-DD')
+const days = 2000
+const maxWidth = 100
+const q = moment().subtract(days, 'days').format('YYYY-MM-DD')
 const coindesk = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${q}&end=${today}`
 
 const bitcoin = async url => {
     const res = await axios.get(url)
     const data = _.flow(
         _.get('data.bpi'),
-        _.values
+        _.values,
+        _.chunk(_.ceil(days/maxWidth)),
+        _.map(l.mean)
     )(res)
     console.log (asciichart.plot (data, { height: 14 }))
     console.log(`\t\t Bitcoin Chart from ${q} to ${today}`)
