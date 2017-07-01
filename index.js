19eb9f23554dbacb7ac5bdc262660178064ca629
Fabian Beuke <mail@beuke.org>
2017-07-01 02:19:36 +0200

    add maxHeight option

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const axios = require('axios')
 const moment = require('moment')
 const asciichart = require ('asciichart')
 
+// Options
+const days = 30      // number of days the chart will go back
+const maxWidth = 100 // max terminal chart width
+const maxHeight = 20 // max terminal chart height
+
 const today = moment().format('YYYY-MM-DD')
-const days = 2000
-const maxWidth = 100
-const q = moment().subtract(days, 'days').format('YYYY-MM-DD')
-const coindesk = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${q}&end=${today}`
+const past = moment().subtract(days, 'days').format('YYYY-MM-DD')
+const coindesk = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${past}&end=${today}`
 
 const bitcoin = async url => {
     const res = await axios.get(url)
@@ -18,8 +21,8 @@ const bitcoin = async url => {
         _.chunk(_.ceil(days/maxWidth)),
         _.map(l.mean)
     )(res)
-    console.log (asciichart.plot (data, { height: 14 }))
-    console.log(`\t\t Bitcoin Chart from ${q} to ${today}`)
+    console.log(asciichart.plot (data, { height: maxHeight }))
+    console.log(`\t\t Bitcoin Chart from ${past} to ${today}`)
 }
 
 bitcoin(coindesk)
