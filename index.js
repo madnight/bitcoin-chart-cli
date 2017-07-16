045fa074b5a36e0d2cc445730be7cf8aeffad21f
Fabian Beuke <mail@beuke.org>
2017-07-16 18:08:13 +0200

    add ethereum, litecoin and ripple

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-const {get, defaultTo, map, flow, at,
-    values, chunk, ceil, mean} = require('lodash/fp')
+const {get, defaultTo, map,
+    flow, remove, chunk, ceil,
+    mean, negate, first, tail} = require('lodash/fp')
 const axios = require('axios')
 const moment = require('moment')
 const asciichart = require ('asciichart')
@@ -9,36 +10,39 @@ const param = require('commander')
 const wrap = require('word-wrap')
 
 param
-    .version('1.0.2')
+    .version('1.1.2')
     .option('-d, --days <n>', 'number of days the chart will go back', parseInt)
     .option('-w, --width <n>', 'max terminal chart width', parseInt)
     .option('-h, --height <n>', 'max terminal chart height', parseInt)
+    .option('-e, --ethereum', 'show ethereum chart instead of bitoin')
+    .option('-l, --litecoin', 'show litecoin chart instead of bitoin')
+    .option('-r, --ripple', 'show ripple chart instead of bitoin')
     .option('--disable-legend', 'disable legend text')
     .parse(process.argv)
 
 const days = defaultTo(90)(param.days)
 const maxWidth = defaultTo(100)(param.width)
 const maxHeight = defaultTo(14)(param.height)
+const coins = [
+    [param.ethereum, 'ETH', 'Ethereum'],
+    [param.litecoin, 'LTC', 'Litecoin'],
+    [param.ripple, 'XRP', 'Ripple'],
+    [true, 'BTC', 'Bitcoin']
+]
 
+const [coin, coinname] = flow(remove(negate(first)), first, tail)(coins)
 const today = moment().format('YYYY-MM-DD')
 const past = moment().subtract(days, 'days').format('YYYY-MM-DD')
-const coindesk = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${past}&end=${today}`
-const coindeskCurrent = 'http://api.coindesk.com/v1/bpi/currentprice.json'
+const coindesk = `https://min-api.cryptocompare.com/data/histoday?fsym=${coin}&tsym=USD&limit=${days}&e=CCCAGG`
+const coindeskCurrent = `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD,EUR`
 
-const current = async url => {
-    const res = await axios.get(url)
-    return flow(
-        get('data.bpi'),
-        at(['USD.rate_float', 'EUR.rate_float']),
-        map(x => Number(x).toFixed(2))
-    )(res)
-}
+const current = async url => (await axios.get(url)).data
 
 const bitcoin = async url => {
     const res = await axios.get(url)
     return flow(
-        get('data.bpi'),
-        values,
+        get('data.Data'),
+        map('close'),
         chunk(ceil(days/maxWidth)),
         map(mean)
     )(res)
@@ -46,10 +50,10 @@ const bitcoin = async url => {
 
 const main = async () => {
     const fetchApi = [bitcoin(coindesk), current(coindeskCurrent)]
-    const [history, [dollar, euro]] = await Promise.all(fetchApi)
+    const [history, {USD, EUR}] = await Promise.all(fetchApi)
     console.log(asciichart.plot (history, { height: maxHeight }))
     if (!param.disableLegend) {
-        const legend = `\t\tBitcoin chart past ${days} days ${past} to ${today}. Current ${dollar}$ / ${euro}€.`
+        const legend = `\t\t${coinname} chart past ${days} days ${past} to ${today}. Current ${USD}$ / ${EUR}€.`
         console.log(wrap(legend, {width: maxWidth, newline: '\n\t\t'}))
     }
 }
