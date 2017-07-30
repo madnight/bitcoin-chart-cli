231f4710d26a7a85b5532b444058d69145957bb2
Fabian Beuke <mail@beuke.org>
2017-07-30 18:12:31 +0200

    add hours and minutes option

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const wrap = require('word-wrap')
 param
     .version('1.1.2')
     .option('-d, --days <n>', 'number of days the chart will go back', parseInt)
+    .option('--hours <n>', 'number of hours the chart will go back', parseInt)
+    .option('--mins <n>', 'number of minutes the chart will go back', parseInt)
     .option('-w, --width <n>', 'max terminal chart width', parseInt)
     .option('-h, --height <n>', 'max terminal chart height', parseInt)
     .option('-e, --ethereum', 'show ethereum chart instead of bitoin')
@@ -29,11 +31,16 @@ const coins = [
     [param.ripple, 'XRP', 'Ripple'],
     [true, 'BTC', 'Bitcoin']
 ]
-
+const time = [
+    [param.mins, 'minutes', 'histominute'],
+    [param.hours, 'hours', 'histohour'],
+    [days, 'days', 'histoday']
+]
+const [timePast, timeName, timeApi] = flow(remove(negate(first)), first)(time)
 const [coin, coinname] = flow(remove(negate(first)), first, tail)(coins)
-const today = moment().format('YYYY-MM-DD')
-const past = moment().subtract(days, 'days').format('YYYY-MM-DD')
-const ccApi = `https://min-api.cryptocompare.com/data/histoday?fsym=${coin}&tsym=USD&limit=${days}&e=CCCAGG`
+const timeFormat = 'YYYY-MM-DD hh:mm a'
+const past = moment().subtract(timePast, timeName).format(timeFormat)
+const ccApi = `https://min-api.cryptocompare.com/data/${timeApi}?fsym=${coin}&tsym=USD&limit=${timePast}&e=CCCAGG`
 const ccApiCurrent = `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD,EUR`
 const current = async url => (await axios.get(url)).data
 const print = string => process.stdout.write(string + '\n')
@@ -51,7 +58,7 @@ const bitcoin = async url => {
 const main = async () => {
     const fetchApi = [bitcoin(ccApi), current(ccApiCurrent)]
     const [history, {USD, EUR}] = await Promise.all(fetchApi)
-    const legend = `\t\t${coinname} chart past ${days} days ${past} to ${today}. Current ${USD}$ / ${EUR}€.`
+    const legend = `\t${coinname} chart past ${timePast} ${timeName} since ${past}. Current ${USD}$ / ${EUR}€.`
     print(asciichart.plot (history, { height: maxHeight }))
     return !param.disableLegend && print(wrap(legend, {width: maxWidth, newline: '\n\t\t'}))
 }
