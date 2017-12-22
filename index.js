05139aec1b119dd5a12f3381be92da9f7e6101c2
Fabian Beuke <mail@beuke.org>
2017-12-22 14:37:17 +0100

    create js module for argument parsing

--- index.js
+++ index.js
@@ -1,58 +1,38 @@
 #!/usr/bin/env node
 
-const {defaultTo, map, flow, sortBy,
-    remove, toLower, trim, negate, first, pad, max }
-                            = require('lodash/fp')
-const moment                = require('moment')
-const asciichart            = require ('asciichart')
-const param                 = require('commander')
-const { version }           = require('./package.json')
-const { interpolateArray }  = require('array-interpolatejs')
-const { CryptoCompareAPI }  = require('./src/CryptoCompareAPI.js')
-const { print, normalize }  = require('./src/utils.js')
+const {map, flow, sortBy, remove, toLower,
+    trim, negate, first, pad, max }
+                             = require('lodash/fp')
+const { days, mins, hours, maxWidth, maxHeight, coin, currency,
+    showCoinList, disableLegend }
+                             = require('./src/arguments.js')
+const moment                 = require('moment')
+const asciichart             = require ('asciichart')
+const { interpolateArray }   = require('array-interpolatejs')
+const { CryptoCompareAPI }   = require('./src/CryptoCompareAPI.js')
+const { print, normalize }   = require('./src/utils.js')
 
-param
-    .version(version)
-    .option('-d, --days <n>', 'number of days the chart will go back', parseInt)
-    .option('--hours <n>', 'number of hours the chart will go back', parseInt)
-    .option('--mins <n>', 'number of minutes the chart will go back', parseInt)
-    .option('-w, --width <n>', 'max terminal chart width', parseInt)
-    .option('-h, --height <n>', 'max terminal chart height', parseInt)
-    .option('-c, --coin <string>', 'specify the coin e.g. ETH (Default: BTC)', 'BTC')
-    .option('--currency <string>', 'specify the trading pair currency (Default: USD)', 'USD')
-    .option('-l, --list', 'list all available coins')
-    .option('--disable-legend', 'disable legend text')
-    .parse(process.argv)
-
-// Parameter defaults
-const days = defaultTo(90)(param.days)
-const maxWidth = defaultTo(100)(param.width)
-const maxHeight = defaultTo(14)(param.height)
-
-// Time interval
 const time = [
-    [param.mins  ,'minutes' ,'histominute'],
-    [param.hours ,'hours'   ,'histohour'],
-    [days        ,'days'    ,'histoday']
+    [mins  ,'minutes' ,'histominute'],
+    [hours ,'hours'   ,'histohour'],
+    [days  ,'days'    ,'histoday']
 ]
 const [timePast, timeName, timeApi] = flow(remove(negate(first)), first)(time)
 const timeFormat = 'YYYY-MM-DD hh:mm a'
 const past = moment().subtract(timePast, timeName).format(timeFormat)
 
 const main = async () => {
-
     const history = interpolateArray(maxWidth)(await CryptoCompareAPI.
-        fetchCoinHistory(timeApi, param.coin, param.currency, timePast))
+        fetchCoinHistory(timeApi, coin, currency, timePast))
 
-    const value = await CryptoCompareAPI.
-        fetchCoinPrice(param.coin, param.currency)
+    const value = await CryptoCompareAPI.fetchCoinPrice(coin, currency)
 
-    const legend = `\t ${param.coin} last ${timePast}`
+    const legend = `\t ${coin} last ${timePast}`
         + ` ${timeName} since ${past}.`
-        + ` Now: ${value[param.currency]} ${param.currency}`
+        + ` Now: ${value[currency]} ${currency}`
 
-    const smallLegend = `\t ${param.coin} last ${timePast}`
-        + ` ${timeName}. Now: ${value[param.currency]} ${param.currency}`
+    const smallLegend = `\t ${coin} last ${timePast}`
+        + ` ${timeName}. Now: ${value[currency]} ${currency}`
 
     const fixed = normalize(max(history))
     const fixedHist = map(x => x.toFixed(fixed))(history)
@@ -65,7 +45,7 @@ const main = async () => {
     }))
 
     return maxWidth < 40 ? false :
-        !param.disableLegend && print(maxWidth < 65 ? smallLegend : legend)
+        !disableLegend && print(maxWidth < 65 ? smallLegend : legend)
 }
 
 const printCoins = async () => {
@@ -76,9 +56,9 @@ const printCoins = async () => {
     )(await CryptoCompareAPI.fetchCoinList())
 }
 
-param.list && printCoins() || main()
+showCoinList && printCoins() || main()
 
 // Coin not found
 process.on('unhandledRejection', () =>
-    print(`Sorry. The coin/currency pair ${param.coin}/${param.currency} `
+    print(`Sorry. The coin/currency pair ${coin}/${currency} `
         + 'does not exist in the cryptocompare api.'))
---
+++ src/arguments.js
@@ -0,0 +1,28 @@
+const param         = require('commander')
+const { version }   = require('../package.json')
+const { defaultTo } = require('lodash/fp')
+
+param
+    .version(version)
+    .option('-d, --days <n>', 'number of days the chart will go back', parseInt)
+    .option('--hours <n>', 'number of hours the chart will go back', parseInt)
+    .option('--mins <n>', 'number of minutes the chart will go back', parseInt)
+    .option('-w, --width <n>', 'max terminal chart width', parseInt)
+    .option('-h, --height <n>', 'max terminal chart height', parseInt)
+    .option('-c, --coin <string>', 'specify the coin e.g. ETH (Default: BTC)', 'BTC')
+    .option('--currency <string>', 'specify the trading pair currency (Default: USD)', 'USD')
+    .option('-l, --list', 'list all available coins')
+    .option('--disable-legend', 'disable legend text')
+    .parse(process.argv)
+
+module.exports = {
+    days: defaultTo(90)(param.days),
+    mins: param.mins,
+    hours: param.hours,
+    coin: param.coin,
+    currency: param.currency,
+    maxWidth: defaultTo(100)(param.width),
+    maxHeight: defaultTo(14)(param.height),
+    showCoinList: param.list,
+    disableLegend: param.disableLegend
+}
