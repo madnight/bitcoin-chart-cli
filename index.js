2ae2fcb35ab8a424bfa934e95d213caa856ef9d2
Fabian Beuke <mail@beuke.org>
2019-08-23 18:57:47 +0200

    reogranize imports

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
-const { map, flow, sortBy, remove, toLower,
-    trim, negate, first, pad, max } = require('lodash/fp')
-const { days, mins, hours, maxWidth, maxHeight, coin, currency,
-    showCoinList, disableLegend }   = require('./src/arguments.js')
-const moment                        = require('moment')
-const asciichart                    = require ('asciichart')
-const { interpolateArray }          = require('array-interpolatejs')
-const { CryptoCompareAPI }          = require('./src/CryptoCompareAPI.js')
-const { print, normalize }          = require('./src/utils.js')
+const args                                   = require('./src/arguments.js')
+const asciichart                             = require('asciichart')
+const moment                                 = require('moment')
+const { map, flow, sortBy, remove, toLower } = require('lodash/fp')
+const { trim, negate, first, pad, max }      = require('lodash/fp')
+const { currency, showCoinList}              = require('./src/arguments.js')
+const { interpolateArray }                   = require('array-interpolatejs')
+const { CryptoCompareAPI }                   = require('./src/CryptoCompareAPI.js')
+const { print, normalize }                   = require('./src/utils.js')
 
 const time = () =>
     flow(remove(negate(first)), first)([
-        [mins  ,'minutes' ,'histominute'],
-        [hours ,'hours'   ,'histohour'],
-        [days  ,'days'    ,'histoday']
+        [args.mins  ,'minutes' ,'histominute'],
+        [args.hours ,'hours'   ,'histohour'],
+        [args.days  ,'days'    ,'histoday']
     ])
 
 const printCoins = async () =>
@@ -27,11 +27,11 @@ const main = async () => {
         .subtract(timePast, timeName)
         .format('YYYY-MM-DD hh:mm a')
 
-    const history = interpolateArray(maxWidth)(await CryptoCompareAPI.
-        fetchCoinHistory(timeApi, coin, currency, timePast))
-    const value = await CryptoCompareAPI.fetchCoinPrice(coin, currency)
+    const history = interpolateArray(args.maxWidth)(await CryptoCompareAPI.
+        fetchCoinHistory(timeApi, args.coin, currency, timePast))
+    const value = await CryptoCompareAPI.fetchCoinPrice(args.coin, currency)
 
-    const baseLegend = `\t ${coin} last ${timePast} ${timeName}`
+    const baseLegend = `\t ${args.coin} last ${timePast} ${timeName}`
     const now = `. Now: ${value[currency]} ${currency}`
     const legend = baseLegend + ` since ${past}` + now
     const smallLegend = baseLegend + now
@@ -41,18 +41,18 @@ const main = async () => {
     const padding = pad(2 + max(fixedHist).toString().length)('')
 
     print(asciichart.plot(fixedHist, {
-        height: maxHeight,
+        height: args.maxHeight,
         padding: padding,
         format: x => (padding + x.toFixed(fixed)).slice(-padding.length)
     }))
 
-    return maxWidth < 40 ? false :
-        !disableLegend && print(maxWidth < 65 ? smallLegend : legend)
+    return args.maxWidth < 40 ? false :
+        !args.disableLegend && print(args.maxWidth < 65 ? smallLegend : legend)
 }
 
 showCoinList && printCoins() || main()
 
 // Coin not found
 process.on('unhandledRejection', () =>
-    print(`Sorry. The coin/currency pair ${coin}/${currency} `
+    print(`Sorry. The coin/currency pair ${args.coin}/${currency} `
         + 'does not exist in the cryptocompare api.'))
