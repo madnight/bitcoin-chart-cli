3cceb4aef4b3bf2f54de327c1dfed30fb80a26dc
Fabian Beuke <mail@beuke.org>
2021-05-11 22:29:51 +0200

    refactor technical-indicator

--- src/technical-indicator.js
+++ src/technical-indicator.js
@@ -61,64 +61,66 @@ const getTechIndicatorColors = () => {
     return indicatorColors;
 };
 
+const printLegend = (name, value) => {
+    const [timePast, timeName, _] = time();
+    print(
+        `\t ${name} ${args.coin} last ${timePast} ${timeName}. Now: ${value}`
+    );
+};
+
+const printRSIChart = (values, padding) => {
+    const [timePast, timeName, _] = time();
+    const rsiPeriod = 14;
+    const rsi = TI.RSI.calculate({
+        period: rsiPeriod,
+        values: values,
+    });
+    const r = interpolate(rsi);
+    print(
+        "\n" +
+            asciichart.plot([r.map((_) => 30), r.map((_) => 70), r], {
+                height: args.maxHeight / 2,
+                max: 70,
+                min: 30,
+                padding: padding,
+                colors: [asciichart.blue, asciichart.blue, asciichart.red],
+            })
+    );
+    printLegend("RSI", r.slice(-1)[0]);
+};
+
+const printMACDChart = (values, padding) => {
+    const [timePast, timeName, _] = time();
+    const rsiPeriod = 14;
+    const m = TI.MACD.calculate({
+        values: values,
+        fastPeriod: 12,
+        slowPeriod: 26,
+        signalPeriod: 9,
+        SimpleMAOscillator: false,
+        SimpleMASignal: false,
+    });
+    const vals = [map("MACD", m), map("signal", m)].map(interpolate);
+    print(
+        "\n" +
+            asciichart.plot(vals, {
+                height: args.maxHeight / 2,
+                padding: padding,
+                colors: [asciichart.blue, asciichart.red],
+            })
+    );
+    printLegend("MACD", vals.slice(-1)[0][0]);
+};
+
 const printTechIndicatorChart = (values, padding) => {
     if (!args.technicalIndicator) return [];
     const [timePast, timeName, _] = time();
     if (args.technicalIndicator.includes("RSI")) {
-        const rsiPeriod = 14;
-        const rsi = TI.RSI.calculate({
-            period: rsiPeriod,
-            values: values,
-        });
-        const r = interpolate(rsi);
-        print(
-            "\n" +
-                asciichart.plot([r.map((_) => 30), r.map((_) => 70), r], {
-                    height: args.maxHeight / 2,
-                    max: 70,
-                    min: 30,
-                    padding: padding,
-                    colors: [asciichart.blue, asciichart.blue, asciichart.red],
-                })
-        );
-
-        print(
-            `\t RSI ${args.coin} last ${timePast} ${timeName}. Now: ${
-                r.slice(-1)[0]
-            }`
-        );
+        printRSIChart(values, padding);
     }
 
     if (args.technicalIndicator.includes("MACD")) {
-        const m = TI.MACD.calculate({
-            values: values,
-            fastPeriod: 12,
-            slowPeriod: 26,
-            signalPeriod: 9,
-            SimpleMAOscillator: false,
-            SimpleMASignal: false,
-        });
-
-        const x = [map("MACD", m), map("signal", m)].map(interpolate)
-        print(
-            "\n" +
-                asciichart.plot(x,
-                    {
-                        height: args.maxHeight / 2,
-                        padding: padding,
-                        colors: [
-                            asciichart.blue,
-                            asciichart.red,
-                        ],
-                    }
-                )
-        );
-
-        print(
-            `\t MACD ${args.coin} last ${timePast} ${timeName}. Now: ${
-                x.slice(-1)[0][0]
-            }`
-        );
+        printMACDChart(values, padding);
     }
 };
 
